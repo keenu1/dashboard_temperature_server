92ae99fec8f240afd3a605ed51fe16a432eeb9c4
keenu1 <nugrohorizki191@gmail.com>
2024-10-28 21:57:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.use((req, res) => {
     res.status(200).send(output);
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
