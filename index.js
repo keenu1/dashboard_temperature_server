4377f82475813195b1d5584cd339a5d9c22bbd13
keenu1 <nugrohorizki191@gmail.com>
2024-10-28 21:01:18 +0700

    update vercel.json

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use(
     require("./src/pages/dashboard/index")
 );
 
-app.get('/test', (req, res) => {
+app.get(`${basePath}/test`, (req, res) => {
     res.send({ status: true, message: "API data route working!" });
 });
 
