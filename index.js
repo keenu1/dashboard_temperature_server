7ae10b4680f95f10fa52f58b3a2034b81b67afe9
keenu1 <nugrohorizki191@gmail.com>
2024-10-29 09:30:08 +0700

    update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = app.listen(PORT, () => {
 });
 
 // Socket.IO setup
-const io = require("socket.io")(PORT2, {
+const io = require("socket.io")(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
